d9620069b2b9533ba7479d0ff18c220e09d7a6c1
Albert William <111026369+suzy969@users.noreply.github.com>
2023-03-01 17:05:22 -0300

    Add files via upload

---
+++ runner.js
@@ -0,0 +1,12 @@
+VanillaTilt.init(document.querySelectorAll(".container"),{
+    max: 3,
+    speed: 400,
+    glare: false,
+    "max-glare": 1,
+});
+VanillaTilt.init(document.querySelectorAll("button"),{
+    max: 8,
+    speed: 400,
+    glare: false,
+    "max-glare": 1,
+});
